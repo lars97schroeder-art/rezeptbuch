d143ddda1acf9095421154bff494cb1fb16a1d6c
Lars Schröder <lars@Larss-MacBook-Pro.local>
2026-07-06 11:11:10 +0200

    Bump service worker cache to v19

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // Bei Änderungen an index.html/app.js/style.css die Versionsnummer erhöhen,
 // damit die Handys die neue App-Version bekommen.
-const SHELL_CACHE = 'rezeptbuch-shell-v18';
+const SHELL_CACHE = 'rezeptbuch-shell-v19';
 const IMG_CACHE = 'rezept-bilder-v1';
 
 const SHELL_FILES = [
